fe200a6199bc8de6e2601fc66ec527142e5660ac
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-12-11 03:04:47 +0000

    split pdf text string into array

--- clever-parser.js
+++ clever-parser.js
@@ -5,11 +5,14 @@ const request = require("request-promise-native");
 const pdfURL = 'http://bcrfc.env.gov.bc.ca/freshet/clever/CLM_FOR_KETT_OKAN_10DAYS.PDF'
 
 async function downloadPDF(pdfURL) {
-  
+  //async operation to read pdf into buffer
   let pdfBuffer = await request.get({uri: pdfURL, encoding: null});
+  
+  //use pdf-parse module to extract text from the pdf
   pdf(pdfBuffer)
-    .then(function(data) {
-      console.log(data)
+    .then((data) => {
+      //do some parsing
+      console.log(data.text.split("\n"));
     })
     .catch((err) => {
       console.log(err.message)
