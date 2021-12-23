ab493a6075ce34bc94b0518db6b6602e632276ac
Spencer Malott <spencermalott16@gmail.com>
2021-12-23 23:48:38 +0000

    make fetchForecast function to return 10-day forcast as structured object

--- clever-csv-parser.js
+++ clever-csv-parser.js
@@ -3,14 +3,31 @@ const axios = require('axios');
 const fetchForecast = function(stationID) {
   return axios.get(`http://bcrfc.env.gov.bc.ca/freshet/clever/${stationID}.CSV`)
     .then((response) => {
-      
-      response.data.split('\r\n').forEach((data) => {
-        console.log(data)
-      })
+      //parse off the header
+      let rawData = response.data.split('\r\n');
+      let currentDay = ''
+
+      const endOfHeaderIndex = rawData.indexOf('DATE,HOUR,FORECAST_DISCHARGE,LOWER_BOUND,UPPER_BOUND') + 1;
+      let streamflowData = {}
+      rawData.splice(endOfHeaderIndex, rawData.length - endOfHeaderIndex - 1).forEach((data, index) => {
+        let hourlyData = data.split(',');
+        let hour = hourlyData[1];
+        let streamflow = hourlyData.splice(2);
+        hourlyData[0] !== '' ? currentDay = hourlyData[0]: currentDay = currentDay
+        console.log(currentDay, hour, streamflow);
+        
+        if (streamflowData[currentDay]) {
+          streamflowData[currentDay][hour] = streamflow;
+        } else {
+          streamflowData[currentDay] = {[hour] : streamflow};
+        }
+
+      });
+      return streamflowData;
     })
     .catch(err => console.log(err.message))
 
 }
 
-fetchForecast('08NL024');
+fetchForecast('08MD013');
   
